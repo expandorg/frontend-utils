ffc84fc37d027a40e7425f08bee0688e910bb938
Slava Sobolev <sobolevslava@gmail.com>
2020-02-21 22:02:23 +0100

    add mixpanel

--- packages/app-utils/analytics.js
+++ packages/app-utils/analytics.js
@@ -2,5 +2,6 @@
 import createAnalyticsMiddlware from './src/analytics/gtm/createAnalyticsMiddlware';
 import GoogleTagManager from './src/analytics/gtm/GoogleTagManager';
 import ua from './src/analytics/gtm/ua';
+import Mixpanel from './src/analytics/mixpanel/Mixpanel';
 
-export { GoogleTagManager, createAnalyticsMiddlware, ua };
+export { GoogleTagManager, createAnalyticsMiddlware, ua, Mixpanel };
